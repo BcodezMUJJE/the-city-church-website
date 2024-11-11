7276d4c389780a7a30b84d0a4d000c61776a62be
BcodezMUJJE <125450966+BcodezMUJJE@users.noreply.github.com>
2024-11-11 08:51:08 +0000

    john three sixteen

--- church-website/src/components/Navbar.js
+++ church-website/src/components/Navbar.js
@@ -6,8 +6,20 @@ import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+  
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -17,6 +29,39 @@ function Navbar() {
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
         </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/news-&-events" className="nav-links" onClick={closeMobileMenu}>
+              News & Events
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact-us" className="nav-links" onClick={closeMobileMenu}>
+              Contact Us
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/give" className="nav-links" onClick={closeMobileMenu}>
+              Give
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about-us" className="nav-links" onClick={closeMobileMenu}>
+              AboutUs
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/join-mc" className="nav-links-mobile" onClick={closeMobileMenu}>
+              Join MC
+            </Link>
+          </li>
+        </ul>
+        {button && <button buttonStyle="btn--outline">JOIN MC</button>}
       </div>
     </nav>
   );
