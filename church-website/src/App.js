0de04337fab5a180deb2ae7f73297cac12522108
BcodezMUJJE <125450966+BcodezMUJJE@users.noreply.github.com>
2025-02-25 17:28:35 +0000

    hghgh

--- church-website/src/App.js
+++ church-website/src/App.js
@@ -2,27 +2,26 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import  Home from './components/pages/Home';
-import NewAndEvents from './components/pages/NewsAndEvents';
+import Home from './components/pages/Home';
+import NewsAndEvents from './components/pages/NewsAndEvents'; // Only import once
 import Give from './components/pages/Give';
 import ContactUs from './components/pages/ContactUs';
 import JoinMc from './components/pages/JoinMc';
-
+import AboutUs from './components/pages/AboutUs';
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/new-and-events' Component={NewAndEvents}/>
-          <Route path='/give' Component={Give}/>
-          <Route path='/contact-us' Component={ContactUs}/>
-          <Route path='/join-mc' Component={JoinMc}/>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/news-and-events' element={<NewsAndEvents />} />
+        <Route path='/give' element={<Give />} /> 
+        <Route path='/contact-us' element={<ContactUs />} />
+        <Route path='/about-us' element={<AboutUs />} /> 
+        <Route path='/join-mc' element={<JoinMc />} /> 
+      </Routes>
+    </Router>
   );
 }
 
