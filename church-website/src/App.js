ac6ced189fcd08c8a051749954511709029c9ce5
BcodezMUJJE <125450966+BcodezMUJJE@users.noreply.github.com>
2025-03-01 12:21:20 +0000

    add more

--- church-website/src/App.js
+++ church-website/src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
-import NewsAndEvents from './components/pages/NewsAndEvents'; // Only import once
+import NewsAndEvents from './components/pages/NewsAndEvents';
 import Give from './components/pages/Give';
 import ContactUs from './components/pages/ContactUs';
 import JoinMc from './components/pages/JoinMc';
 import AboutUs from './components/pages/AboutUs';
 import Cli from './components/Cli';
+import { ServiceTimes } from './components/Time';
+import { RoundTableDetails } from './components/WeeklyRoundTable';
+import Tcg from './components/Tcg';
 
 const App = () => {
   return (
@@ -22,6 +25,9 @@ const App = () => {
         <Route path='/about-us' element={<AboutUs />} /> 
         <Route path='/join-mc' element={<JoinMc />} />
         <Route path="/Cli" element={<Cli />} />
+        <Route path="/service-times" element={<ServiceTimes />} />
+        <Route path="/weekly-round-table" element={<RoundTableDetails />} />
+        <Route path="/tcg-page" element={<Tcg />} />
       </Routes>
     </Router>
   );
