6f5e24a80026ec002f0409f26c68203608d56aaa
BcodezMUJJE <125450966+BcodezMUJJE@users.noreply.github.com>
2025-03-01 12:20:50 +0000

    added more

--- church-website/src/components/Footer.js
+++ church-website/src/components/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
       {/* Top Section */}
       <div className="top">
         <div>
-          <h1>THE CITY CHURCH LUZIRA</h1>
+          <h1>THE CITY CHURCH</h1>
           <p>Come And Worship With Us</p>
         </div>
         {/* Social Media Icons */}
@@ -42,32 +42,21 @@ function Footer() {
       <div className="bottom">
         <div>
           <h4>REACH OUT</h4>
-          <a href="/">1. Support A Mission</a>
-          <a href="/">2. Partner With Us</a>
-          <a href="/">3. Need Prayer</a>
-          <a href="/">4.Sign Up</a>
-        </div>
-        <div>
-          <h4>OUR COMMUNITY</h4>
-          <a href="https://www.facebook.com/TheeCityChurch/">Facebook</a>
-          <a href="https://www.instagram.com/theecitychurch/">Instagram</a>
-          <a href="https://x.com/TheCityChurchL/">Twitter</a>
-          <a href="https://www.linkedin.com/company/the-city-church-luzira/">LinkedIn</a>
-          <a href="https://www.youtube.com/@thecitychurch-luzira2585/">YouTube</a>
-          <a href="https://theecitychurchluzira.podbean.com/">Podbean</a>
+          <a href="/">Support A Mission</a>
+          <a href="/">Partner With Us</a>
+          <a href="/">Need Prayer</a>
         </div>
         <div>
           <h4>MISSIONS</h4>
           <a href="/">The City Leadership Institute</a>
           <a href="/">The Fields City International School</a>
-          <a href="/">Happy Hearts Primary School</a>
           <a href="https://www.transformafricanministries.org/">Transform African Ministries</a>
         </div>
         <div>
           <h4>RESOURCES</h4>
-          <a href="/">1.First Time Guests (Next steps class notes?)</a>
-          <a href="/">2.Sermons</a>
-          <a href="/">3.Books</a>
+          <a href="/">First Time Guests (Next steps classes)</a>
+          <a href="/">Sermons</a>
+          <a href="/">Books</a>
         </div>
       </div>
     </div>
