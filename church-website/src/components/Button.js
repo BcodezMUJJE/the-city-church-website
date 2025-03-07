0c5da0af80574df1c980e0cfdf27ea18fc3d6082
BcodezMUJJE <125450966+BcodezMUJJE@users.noreply.github.com>
2025-03-07 09:55:12 +0000

    adding more

--- church-website/src/components/Button.js
+++ church-website/src/components/Button.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Button.css";
 
 const STYLES = ["btn--primary", "btn--outline"];
-
 const SIZES = ["btn--medium", "btn--large", "btn--mobile", "btn--wide"];
 
 export const Button = ({ children, type, onClick, buttonStyle, buttonSize }) => {
@@ -11,10 +9,15 @@ export const Button = ({ children, type, onClick, buttonStyle, buttonSize }) => 
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
     return (
-        <Link to="/sign-up" className="btn-mobile">
+        <a 
+            href="https://www.youtube.com/@thecitychurch-luzira2585" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="btn-mobile"
+        >
             <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
                 {children}
             </button>
-        </Link>
+        </a>
     );
 };
