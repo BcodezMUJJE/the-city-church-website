62728d3789098f4a4e8bbc589e5e0362e8dc0906
BcodezMUJJE <125450966+BcodezMUJJE@users.noreply.github.com>
2025-05-26 09:39:35 +0000

    more

--- church-website/src/components/BookLibrary.js
+++ church-website/src/components/BookLibrary.js
@@ -13,14 +13,8 @@ const books = [
   { id: 2, title: "Unhindered", author: "Dr. Alex Mutagubya", description: "Overcoming The Control And Influence Of The Spirit.", image: img4 },
   { id: 3, title: "Ministry DNA", author: "Dr. Alex Mutagubya", description: "A guide to understanding God’s purpose for your life.", image: img5 },
   { id: 4, title: "Marriage For The Unmarried", author: "Dr. Alex Mutagubya", description: "A journalist investigates the evidence for Jesus.", image: img1 },
-  { id: 5, title: "Knowing God", author: "J.I. Packer", description: "A deep dive into the nature and character of God.", image: img1 },
   { id: 6, title: "Digital Discipleship", author: "Bwire Felix", description: "A Journey In Church Media.", image: img2 },
   { id: 7, title: "Through A Mum's Eyes", author: "Abote Priscilla Maloba", description: " Life Lessons for my son", image: img3 },
-  { id: 8, title: "The Screwtape Letters", author: "C.S. Lewis", description: "A series of letters from a senior demon to a junior demon.", image: img1 },
-  { id: 9, title: "The Great Divorce", author: "C.S. Lewis", description: "A theological dream vision about heaven and hell.", image: img1 },
-  { id: 10, title: "The Pursuit of God", author: "A.W. Tozer", description: "A Christian classic on the pursuit of intimacy with God.", image: img1 },
-  { id: 11, title: "The Cost of Discipleship", author: "Dietrich Bonhoeffer", description: "A challenging book on what it means to be a disciple of Christ.", image: img1 },
-  { id: 12, title: "Desiring God", author: "John Piper", description: "A book on Christian hedonism and the pursuit of joy in God.", image: img1 },
 ];
 
 // SearchBar Component
