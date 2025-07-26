6a245e54dca7ecfd371aac47355f6d65b74a2c44
BillHax <125450966+BcodezMUJJE@users.noreply.github.com>
2025-07-26 15:49:16 +0000

    done working on the library

--- church-website/src/components/BookLibrary.js
+++ church-website/src/components/BookLibrary.js
@@ -3,9 +3,17 @@ import "./BookLibrary.css";
 import Footer from "./Footer"; // Import Footer component
 import img1 from "../Assets/boookkk.jpg";
 import img2 from "../Assets/Bk_Fel.png";
-import img3 from "../Assets/Bk_Prie.png"; // Uncomment if needed
-import img4 from "../Assets/Bk_Alex.jpg"; // Uncomment if needed
-import img5 from "../Assets/Bk_AL3.jpg"; // Uncomment if needed
+import img3 from "../Assets/Bk_Prie.png"; 
+import img4 from "../Assets/Bk_Alex.jpg"; 
+import img5 from "../Assets/Bk_AL3.jpg"; 
+import img6 from "../Assets/Derick.jpeg"; 
+import img7 from "../Assets/Lydia.png"; 
+import img8 from "../Assets/Gibu.png"; 
+import img9 from "../Assets/Baale.png"; 
+import img10 from "../Assets/Moris.png"; 
+import img11 from "../Assets/Tushabe.png"; 
+import img12 from "../Assets/Wamibi.png";
+import img13 from "../Assets/Mudeda.png";
 
 // Sample Data with Image URLs
 const books = [
@@ -15,6 +23,14 @@ const books = [
   { id: 4, title: "Marriage For The Unmarried", author: "Dr. Alex Mutagubya", description: "A journalist investigates the evidence for Jesus.", image: img1 },
   { id: 6, title: "Digital Discipleship", author: "Bwire Felix", description: "A Journey In Church Media.", image: img2 },
   { id: 7, title: "Through A Mum's Eyes", author: "Abote Priscilla Maloba", description: " Life Lessons for my son", image: img3 },
+  { id: 8, title: "Over Coming Fear", author: "Derick Ongeiwun", description: "A guide to overcome your fears.", image: img6 },
+  { id: 9, title: "The Art Of Serving Children", author: "Lydia Biira Kuhinira", description: "The world of children is a realm of endless curiosity, imagination and wonder", image: img7 },
+  { id: 10, title: "Fruitfullness", author: "Gerald W. Gibu", description: "The call for every child of God.", image: img8 },
+  { id: 11, title: "The Wilderness Experience", author: "Herny Baale", description: "A powerful book that talks about tough times in life.", image: img9 },
+  { id: 12, title: "Godly Relatioships Leading to Marriage", author: "Morris Okwakol", description: "A Biblical perspective in today's contemporary relationships.", image: img10 },
+  { id: 13, title: "The Battle Field", author: "Emmanuel Tushabe", description: "The Journey Of a Believer", image: img11 },
+  { id: 14, title: "The Enemy Within", author: "David Wamimbi Mulaamya", description: "Victory Over Self", image: img12 },
+  { id: 15, title: "Complete The Task", author: "Derick Mudeda", description: "Defying The Urge To Quit", image: img13 },
 ];
 
 // SearchBar Component
